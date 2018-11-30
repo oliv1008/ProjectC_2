fa00ba60bc2feabf45785d111b47dfb81c38821b
oliv1008 <omillochau@gmail.com>
2018-11-30 17:53:25 +0100

    Verification des arguments au lancement du programme

--- main.c
+++ main.c
@@ -4,9 +4,24 @@
 int main(int argc, char **argv)
 {
 	char c;
+	FILE * f;
 	
-	FILE * f = fopen("/home/olivier/Bureau/Polytech/ProjetC_2/MKP-Instances/_mknapcb1_res.txt", "r");
-	Parser(f);
+	if (argc != 2)
+	{
+		printf("Erreur, veuillez spécifier un chemin vers un fichier .txt\n");
+	}
+	else
+	{
+		f = fopen(argv[1], "r");
+		if (f == NULL)
+		{
+			printf("Chemin d'accès incorrect, veuillez spécifier un chemin d'accès correct\n");
+		}
+		else
+		{
+			Parser(f);
+		}
+	}
 	
 	//Solution temp pour empêcher le terminal de se fermer
 	scanf("%c", &c);
