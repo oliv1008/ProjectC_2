0197584bf1c87b332770af3eeb530353fb7ede37
laurie44 <omillochau@gmail.com>
2019-01-07 11:39:16 +0100

    début métaheuristiques

--- source/recherche_locale.c
+++ source/recherche_locale.c
@@ -3,8 +3,59 @@
 #include "include/codage.h"
 #include "include/heuristique.h"
 
-// POUR LE CODAGE DIRECT
-// (1) on ajoute un objet (0 devient 1), tester les N possibilités et garder que celles réalisables.
+// FONCTIONS D'OPERATEURS DE VOISINAGE
+// CODAGE DIRECT
+// (1)
+
+Solution ** ajoutObject_indirect(Instance * Instance)
+{
+	
+	Solution * Algorithme_solutions (Instance * instance, Object ** (*ordo_fct)(Instance *), int codage)
+	// on prend une solution qui existe
+	// on remplace un 0 par un 1
+	// si c'est réalisable, on garde dans une liste
+	// sinon on tej
+	
+	int i = 0;
+	int boolC = 0;
+	
+	while (i != solution->nbObject-1 || boolC != 1)
+	{
+		// on parcourt la liste tant que l'on a pas trouvé un objet qui n'est pas dans le sac
+		if (solution->objectTab[i] == 1)
+		{
+			i++;
+		}
+		else
+		{
+			solution->objectTab[i] = 1;
+			boolC = 1;
+		}
+	}
+	
+	// on vérifie si cette solution est réalisable
+	
+	for (int j = 0; j < solution->nbDimension; j++)
+	{
+		if (solution->weightDimension[j] > i)
+	}
+	for (int i = 0; i < instance->nbDimension; i++)
+	{
+		if (solution->weightDimension[i] > instance->limit[i])
+		{
+			return 0;
+		}
+	}
+
+	return 1;
+	
+	
+	
+	
+}
+
+
+
 // (2) on échange un objet (0 devient 1 et 1 devient 0), solutions voisines sont contituées des solutions réalisables résultantes de tous les échanges possibles.
 
 
